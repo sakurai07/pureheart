f406fca077e7bb788d79439f6419399e588a8ca6
3417 <3417@example.com>
2024-10-08 15:10:00 +0900

    関連付け・サインアップ

--- app/models/block.rb
+++ app/models/block.rb
@@ -1,2 +1,6 @@
 class Block < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :blocked_user, class_name:"User", foreign_key:"blocked_user_id"
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,14 @@ class User < ApplicationRecord
 
   validates :hobby_1, presence: true
 
+  #関連付け
+  has_many :follows, dependent: :destroy
+  has_many :followeds,class_name:"Follow", foreign_key: "followed_user_id"
+  has_many :blocks, dependent: :destroy
+  has_many :blockeds,class_name:"Block", foreign_key: "blocked_user_id"
+
+  enum grade: {"1年": 1,"2年": 2,"3年": 3}
+  enum klass: {"1組": 1,"2組": 2,"3組": 3,"4組": 4}
+
+
 end

b759930bafdeeb721f36657ccbbc5cfbea7c1a8f
3416 <3416@example.com>
2024-10-15 14:38:59 +0900

    更新

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -36,7 +36,37 @@ class UserController < ApplicationController
     send_data(user.icon, disposition: :inline)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user = update(user_update_attributes)
+      redirect_to profile_path(@user), notice: "User was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def user_params_update
+    params.require(:user).permit(:name, :icon)
+  end
+
+  def user_update_attributes
+    if user_params_update[:icon] == nil
+    {
+      name: user_params_update[:name]
+    }
+    else
+    {
+      name: user_params_update[:name], icon: user_params_update[:icon].read
+    }
+    end
+  end
+
     def user_params
       params.require(:user).permit(:user_name, :password, :password_confirmartion, :name, :grade, :klass, :icon, :profile, :hobby_1)
     end

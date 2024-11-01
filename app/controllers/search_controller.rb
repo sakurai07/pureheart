cfe00f5ebc7967b93fe110e6538ded6073e9180f
3438 <3438@example.com>
2024-11-01 11:31:37 +0900

    学年と組検索

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,13 @@ class SearchController < ApplicationController
   def result
     if params[:search].present? && params[:search][:hobby].present?
       @user = User.where("hobby_1 like '%#{params[:search][:hobby]}'").order(created_at: :desc)
+    elsif params[:search].present? && params[:search][:grade].present? && params[:search][:klass].present?
+      @user = User.where("grade == #{params[:search][:grade]} and klass == #{params[:search][:klass]}").order(created_at: :desc)
+      # if params[:search].present? && params[:search][:grade].present?
+      #   @user = User.where("grade like '%#{params[:search][:grade]}'").order(created_at: :desc)
+      # # elsif params[:search].present? && params[:search][:klass].present?
+      # #   @user = User.where("klass like '%#{params[:search][:klass]}'").order(created_at: :desc)
+      # end
     else
       @user = User.all.order(created_at: :desc)
     end

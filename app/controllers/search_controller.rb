547c9b2d227bff80803aea20e65856505f280e76
3438 <3438@example.com>
2024-12-10 14:16:59 +0900

    検索機能改善

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,15 @@
 class SearchController < ApplicationController
   def result
-    if params[:search].present? && params[:search][:hobby].present?
-      @user = User.where("id <> #{current_user.id} and (hobby_1 like '%#{params[:search][:hobby]}' or hobby_2 like '%#{params[:search][:hobby]}' or hobby_3 like '%#{params[:search][:hobby]}' or hobby_4 like '%#{params[:search][:hobby]}' or hobby_5 like '%#{params[:search][:hobby]}')").order(created_at: :desc)
-    elsif params[:search].present? && params[:search][:grade].present? && params[:search][:klass].present?
+    if params[:search].present? && params[:search][:hobby].present? && params[:search][:grade].present? && params[:search][:klass].present?
+      @user = User.where("id <> #{current_user.id} and (hobby_1 like '%#{params[:search][:hobby]}' or hobby_2 like '%#{params[:search][:hobby]}' or hobby_3 like '%#{params[:search][:hobby]}' or hobby_4 like '%#{params[:search][:hobby]}' or hobby_5 like '%#{params[:search][:hobby]}') and (grade like '%#{params[:search][:grade]}' and klass like '%#{params[:search][:klass]}') ").order(created_at: :desc)
+    elsif params[:search].present? && params[:search][:grade].present? && params[:search][:klass].present?#学年と組一致
       @user = User.where("grade = #{params[:search][:grade]} and klass = #{params[:search][:klass]} and id <> #{current_user.id}").order(created_at: :desc)
-    elsif params[:search].present? && params[:search][:grade].present?
+    elsif params[:search].present? && params[:search][:grade].present?#学年が一致
       @user = User.where("id <> #{current_user.id} and grade like '%#{params[:search][:grade]}'").order(created_at: :desc)
-    elsif params[:search].present? && params[:search][:klass].present?
+    elsif params[:search].present? && params[:search][:klass].present?#組が一致
       @user = User.where("id <> #{current_user.id} and klass like '%#{params[:search][:klass]}'").order(created_at: :desc)
+    elsif params[:search].present? && params[:search][:hobby].present?#趣味一致
+      @user = User.where("id <> #{current_user.id} and (hobby_1 like '%#{params[:search][:hobby]}' or hobby_2 like '%#{params[:search][:hobby]}' or hobby_3 like '%#{params[:search][:hobby]}' or hobby_4 like '%#{params[:search][:hobby]}' or hobby_5 like '%#{params[:search][:hobby]}')").order(created_at: :desc) 
     else
       @user = []
       flash.now[:danger] = "検索条件を入力してください"
